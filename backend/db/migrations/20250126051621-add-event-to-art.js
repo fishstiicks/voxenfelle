e5624e29fdc175826d98314513fc949faad3287d
fishstiicks <loketteshimmer@gmail.com>
2025-01-25 23:29:20 -0600

    add event to art

--- backend/db/migrations/20250126051621-add-event-to-art.js
+++ backend/db/migrations/20250126051621-add-event-to-art.js
@@ -1,14 +1,25 @@
 'use strict';
 
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('Arts', 'event', {
-      type: Sequelize.STRING,
-      allowNull: true,
-    });
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn(
+      { schema: options.schema, tableName: 'Arts' },
+      'event', {
+        type: Sequelize.STRING,
+        allowNull: true,
+      }
+    );
   },
 
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeColumn('Arts', 'event');
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn(
+      { schema: options.schema, tableName: 'Arts' },
+      'event'
+    );
   }
 };
